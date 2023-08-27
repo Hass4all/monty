89713aa5060466bee6ff71828686dec269192e61
hass4all <hass4all@hotmail.com>
2023-08-27 05:53:22 -0700

    .

---
+++ pint.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * @head: stack head
+ * @counter: line_number
+ * Ret: no return
+*/
+void f_pint(stack_t **head, unsigned int counter)
+{
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*head)->n);
+}
---
+++ stack.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+ * @head: stack head
+ * @counter: line_number
+ * Ret: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
